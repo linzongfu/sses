c86e24e5a86d85fdc1b2ff4b90f5bf9c1cd18256
linzongfu <2539408294@qq.com>
2018-02-06 22:33:25 +0800

    laravel init 6

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -23,8 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = Auth::id();
-        echo $user;
         return view('home');
     }
 }
